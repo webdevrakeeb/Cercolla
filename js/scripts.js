354aa218c768d257c1523ed7a41e350e661dc4e4
webdevrakeeb <developerrakeeb01@gmail.com>
2025-04-08 14:25:01 +0600

    Complete User Login And Registration Popup Form and also add Mobile Menu Card

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,38 @@
 			}
 		});
 
+		//User Log In Register PopUp Handler
+		jQuery('.register-now-btn-card p span').click(function(){
+			jQuery('.modal-header').addClass('hide-area');
+			jQuery('.login-form-card').addClass('hide-area');
+			jQuery('.registration-form-card').addClass('show-area');
+		})
+		jQuery('.back-login-btn p').click(function(){
+			jQuery('.modal-header').removeClass('hide-area');
+			jQuery('.login-form-card').removeClass('hide-area');
+			jQuery('.registration-form-card').removeClass('show-area');
+		})
+
+		// Mobile Menu Tab Handler
+		$('.tab-menu li').click(function(){
+			var tab_id = $(this).data('tab');
+			$('.tab-menu li').removeClass('active');
+			$('.tab-content').removeClass('active');
+			$(this).addClass('active');
+			$("#" + tab_id).addClass('active');
+		});
+
+		// Mobile User Login Registration Handler
+		jQuery('.aside-register-now-btn-card p').click(function(){
+			jQuery('.login-form-card').addClass('hide-area');
+			jQuery('.registration-form-card').addClass('show-area');
+		})
+		// Mobile User Login Registration Handler
+		jQuery('#aside-login-back-btn p').click(function(){
+			jQuery('.login-form-card').removeClass('hide-area');
+			jQuery('.registration-form-card').removeClass('show-area');
+		})
+
 
 		
 
